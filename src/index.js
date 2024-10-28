4bb0e6c145e429112a89856529efc66936a06919
Yorqin Rahimboboyev <crossrick88@gmail.com>
2024-10-28 19:56:03 +0500

    Styling with Material UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import App from './components/App';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
