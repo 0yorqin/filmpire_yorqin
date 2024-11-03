40cdce7a9e7347a6bf7c9ec48282ab04f16a6631
Yorqin Rahimboboyev <crossrick88@gmail.com>
2024-11-03 12:36:17 +0500

    Movie information page Part 1

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,7 +1,112 @@
 import React from 'react';
+import {
+  Modal,
+  Typography,
+  Box,
+  Grid2,
+  Button,
+  Rating,
+  ButtonGroup,
+  CircularProgress,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { useGetMovieQuery } from '../../services/TMDB';
+import useStyles from './styles';
+import genreIcons from '../../assets/genres';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 const MovieInformation = () => {
-  return <div>MovieInformation</div>;
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something went wrong... Go back</Link>
+      </Box>
+    );
+  }
+
+  return (
+    <Grid2 container className={classes.containerSpaceAround}>
+      <Grid2 item sm={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid2>
+      <Grid2 item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid2 item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} precision={0.1} />
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              style={{ marginLeft: '10px' }}
+            >
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime} min
+            {data?.spoken_languages.length > 0
+              ? ` / ${data?.spoken_languages[0].name}`
+              : ''}
+          </Typography>
+        </Grid2>
+        <Grid2 item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link
+              className={classes.links}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+              key={genre.name}
+            >
+              <img
+                src={genreIcons[genre.name.toLowerCase()]}
+                alt=""
+                className={classes.genreImage}
+                height={30}
+              />
+              <Typography color="textPrimary" variant="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid2>
+      </Grid2>
+    </Grid2>
+  );
 };
 
 export default MovieInformation;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { userSelector } from '../../features/auth';
 import { useSelector } from 'react-redux';
+import { Typography, Box, Button } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
-  return <div>Profile - {user.username}</div>;
+  const favouriteMovies = [];
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favouriteMovies.length ? (
+        <Typography variant="h5">
+          Add your favourite movies to see them here.
+        </Typography>
+      ) : (
+        <Box>Favourite Movies</Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
